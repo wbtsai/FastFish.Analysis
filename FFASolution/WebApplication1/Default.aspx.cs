b79e7945dd681b7a08f2f507f050a33534e7a9d9
wbtsai <wbtsai@msn.com>
2012-08-05 23:57:24 +0800

    Add sample test

--- FFASolution/WebApplication1/Default.aspx.cs
+++ FFASolution/WebApplication1/Default.aspx.cs
@@ -6,6 +6,10 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using FF.Analysis.Core.Docx;
 
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+
 namespace WebApplication1
 {
     public partial class _Default : System.Web.UI.Page
@@ -15,7 +19,11 @@ namespace WebApplication1
             //
             if (!this.IsPostBack)
             {
-                DocHelper.Create("c:\\temp\\template.docx","c:\\temp\\aaa-1.docx");
+                //DocHelper.Create("c:\\temp\\template.docx","c:\\temp\\aaa-1.docx");
+                List<SdtBlock> list = DocHelper.GetTemplateBlocks();
+
+                int count = list.Count;
+
             }
         }
     }
