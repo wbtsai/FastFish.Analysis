1887996a6d77a293601aee5f8673555895072e0e
nick <wbtsai@msn.com>
2012-08-21 09:29:11 +0800

    Add FF.DocBuilder Project Add Library log4net ,commandLineParser

---
+++ FFASolution/DocBuilder/Options/Option.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Option.cs" company="yuanta">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace DocBuilder.Options
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using CommandLine;
+    using CommandLine.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Option
+    {
+
+        [Option("i",null, Required = true, HelpText = "xml config file to read.")]
+        public string InputFile { get; set; }
+        
+        [Option("v", null, HelpText = "Print details during execution.")]
+        public bool Verbose { get; set; }
+                
+        [HelpOption]
+        public string GetUsage()
+        {
+            var usage = new StringBuilder();
+            usage.AppendLine("Doc Builder Application 1.0");
+            usage.AppendLine("Read user manual for usage instructions...");
+            return usage.ToString();
+        }
+
+    }
+}
---
+++ FFASolution/DocBuilder/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using DocBuilder.Options;
+
+namespace DocBuilder
+{
+    /// <summary>
+    /// Doc Builder Console Application
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var options = new Option();
+
+                
+                
+                ICommandLineParser parser = new CommandLineParser();
+                if (parser.ParseArguments(args, options))
+                {
+                    if (options.Verbose)
+                    {
+                        Console.WriteLine(options.InputFile);
+                    }
+                    else
+                        Console.WriteLine("working ...");
+                }
+
+                throw new Exception("This is a test");
+
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("程式發現不可預期之錯誤：");
+
+                Console.WriteLine("Exception=" + DateTime.Now.ToString("yyyy/MM/dd"));
+
+                Console.WriteLine(ex.InnerException + ex.Message + ex.StackTrace);
+                
+                Environment.Exit(-1);
+            }
+           
+        }
+    }
+}
