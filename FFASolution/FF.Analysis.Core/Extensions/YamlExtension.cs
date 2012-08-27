7dd0ea1c8660764ceb170fe6577e4b5bad8680b8
wbtsai <wbtsai@msn.com>
2012-08-27 23:21:26 +0800

    Add File

---
+++ FFASolution/FF.Analysis.Core/Extensions/YamlExtension.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="YamlExtension.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace FF.Analysis.Core.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class YamlExtension
+    {
+    }
+}
