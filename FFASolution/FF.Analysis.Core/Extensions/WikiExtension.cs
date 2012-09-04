f4ea595fb9de8b8ac6f2a621970a9ce19d93c9fe
nick <wbtsai@msn.com>
2012-09-04 13:01:27 +0800

    Add WikiExtension.cs

---
+++ FFASolution/FF.Analysis.Core/Extensions/WikiExtension.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="WikiExtension.cs" company="yuanta">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace FF.Analysis.Core.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public static class WikiExtension
+    {
+
+
+    }
+}
