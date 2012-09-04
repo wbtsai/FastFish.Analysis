d6b6e85ed5ae2218d525fae5b9fae049ebe97482
nick <wbtsai@msn.com>
2012-09-04 14:07:02 +0800

    Add Wikiplex to projects

--- FFASolution/FF.Analysis.Core/Extensions/WikiExtension.cs
+++ FFASolution/FF.Analysis.Core/Extensions/WikiExtension.cs
@@ -10,13 +10,34 @@ namespace FF.Analysis.Core.Extensions
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using WikiPlex;
 
     /// <summary>
     /// TODO: Update summary.
     /// </summary>
     public static class WikiExtension
     {
+        /// <summary>
+        /// Render Wiki To Html
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string WikiRenderToHtml(this string text)
+        {
+            var engine = new WikiPlex.WikiEngine();
+            string output = engine.Render(text);
 
+            return output;
+        }
 
+        /// <summary>
+        /// Render wiki text to docx
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string WikiRenderToDocx(this string text)
+        {
+            return string.Empty;
+        }
     }
 }
